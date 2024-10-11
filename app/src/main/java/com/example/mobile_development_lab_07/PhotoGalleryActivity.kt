72c1ff20df68a8bc81c959c4191eccd7eae1a62b
Merch04 <kirill004mel@mail.ru>
2024-10-11 15:00:54 +0700

    Start project

---
+++ app/src/main/java/com/example/mobile_development_lab_07/PhotoGalleryActivity.kt
@@ -0,0 +1,22 @@
+package com.example.mobile_development_lab_07
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class PhotoGalleryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_photo_gallery)
+        val isFragmentContainerEmpty =
+            savedInstanceState == null
+        if (isFragmentContainerEmpty) {
+            supportFragmentManager
+                .beginTransaction()
+//                .add(R.id.fragmentContainer, PhotoGalleryFragment.newInstance())
+                .commit()
+        }
+    }
+}
