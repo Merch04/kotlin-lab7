86d9cb5ca76f57d8fafa447f1da622115a6cbeee
Merch04 <kirill004mel@mail.ru>
2024-10-13 13:32:35 +0700

    change api

--- app/src/main/java/com/example/mobile_development_lab_07/api/FlickrApi.kt
+++ app/src/main/java/com/example/mobile_development_lab_07/api/FlickrApi.kt
@@ -36,15 +36,19 @@ import retrofit2.http.Query
 import retrofit2.http.Url
 
 interface FlickrApi {
-    // Fetch photos using query parameters
     @GET("services/rest/")
     fun fetchPhotos(
-        @Query("method") method: String = "flickr.interestingness.getList",
-        @Query("api_key") apiKey: String = "08d7a58a4f8a9d0e7842cc9caebcd60a",
-        @Query("format") format: String = "json",
-        @Query("nojsoncallback") noJsonCallback: Int = 1,
-        @Query("extras") extras: String = "url_s"
+        @Query("method") method: String = "flickr.interestingness.getList"
     ): Call<FlickrResponse>
+    // Fetch photos using query parameters
+//    @GET("services/rest/")
+//    fun fetchPhotos(
+//        @Query("method") method: String = "flickr.interestingness.getList",
+//        @Query("api_key") apiKey: String = "08d7a58a4f8a9d0e7842cc9caebcd60a",
+//        @Query("format") format: String = "json",
+//        @Query("nojsoncallback") noJsonCallback: Int = 1,
+//        @Query("extras") extras: String = "url_s"
+//    ): Call<FlickrResponse>
 //    fun fetchPhotos(
 //        @Query("method") method: String = "flickr.photos.search",
 //        @Query("api_key") apiKey: String = "08d7a58a4f8a9d0e7842cc9caebcd60a",
@@ -59,4 +63,10 @@ interface FlickrApi {
 
     @GET
     fun fetchUrlBytes(@Url url: String): Call<ResponseBody>
+
+    @GET("services/rest/")
+    fun searchPhotos(
+        @Query("method") method: String = "flickr.interestingness.getList",
+        @Query("text") text: String,
+    ): Call<FlickrResponse>
 }
