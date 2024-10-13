179788c851163fb163ec994d443e56a98145064a
Merch04 <kirill004mel@mail.ru>
2024-10-13 13:01:38 +0700

    changes

--- app/src/main/java/com/example/mobile_development_lab_07/PhotoGalleryActivity.kt
+++ app/src/main/java/com/example/mobile_development_lab_07/PhotoGalleryActivity.kt
@@ -1,20 +1,16 @@
 package com.example.mobile_development_lab_07
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.os.StrictMode
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.example.mobile_development_lab_07.api.FlickrApi
-import retrofit2.Call
-import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
+
 
 class PhotoGalleryActivity :
     AppCompatActivity() {
     override fun onCreate(savedInstanceState:
                           Bundle?) {
         super.onCreate(savedInstanceState)
+        StrictMode.enableDefaults()
         setContentView(R.layout.activity_photo_gallery)
         val isFragmentContainerEmpty =
             savedInstanceState == null
