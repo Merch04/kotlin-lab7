f5873e7a9e2d96ac4e145f8a06f81ce75a04fbc2
Merch04 <kirill004mel@mail.ru>
2024-10-13 07:14:09 +0700

    update PhotoGalleryFragment.kt

--- app/src/main/java/com/example/mobile_development_lab_07/PhotoGalleryFragment.kt
+++ app/src/main/java/com/example/mobile_development_lab_07/PhotoGalleryFragment.kt
@@ -6,17 +6,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.mobile_development_lab_07.api.FlickrApi
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
+
 
 private const val TAG = "PhotoGalleryFragment"
 
@@ -39,6 +33,15 @@ class PhotoGalleryFragment : Fragment() {
             GridLayoutManager(context, 3)
         return view
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        photoGalleryViewModel.galleryItemLiveData.observe(viewLifecycleOwner) { galleryItems ->
+            Log.d(TAG, "Have gallery items from ViewModel $galleryItems")  // Обновить данные, поддерживающие представление утилизатора
+        }
+    }
+
     companion object {
         fun newInstance() =
             PhotoGalleryFragment()
