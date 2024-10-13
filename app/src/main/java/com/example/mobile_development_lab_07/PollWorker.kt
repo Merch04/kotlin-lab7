4ec5f80b272d566118319f7793960333c0c899d8
Merch04 <kirill004mel@mail.ru>
2024-10-13 16:50:27 +0700

    fixes

--- app/src/main/java/com/example/mobile_development_lab_07/PollWorker.kt
+++ app/src/main/java/com/example/mobile_development_lab_07/PollWorker.kt
@@ -1,4 +1,15 @@
 package com.example.mobile_development_lab_07
 
-class PollWorker {
+import android.content.Context
+import android.util.Log
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+
+private const val TAG = "PollWorker"
+class PollWorker(private val context: Context, workerParams: WorkerParameters)
+    : Worker(context, workerParams) {
+    override fun doWork(): Result {
+        Log.i(TAG, "Work request triggered")
+        return Result.success()
+    }
 }
