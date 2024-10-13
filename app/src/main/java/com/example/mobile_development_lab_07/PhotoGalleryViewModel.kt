a7005275fc7604515bcae242ae6e426508a1288f
Merch04 <kirill004mel@mail.ru>
2024-10-13 07:09:10 +0700

    add PhotoGalleryViewModel.kt

---
+++ app/src/main/java/com/example/mobile_development_lab_07/PhotoGalleryViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.mobile_development_lab_07
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+
+class PhotoGalleryViewModel : ViewModel() {
+    val galleryItemLiveData:
+            LiveData<List<GalleryItem>>
+    init {
+        galleryItemLiveData =
+            FlickrFetcher().fetchPhotos()
+    }
+}
