d6110ac82ece1a5d09f7dc8ec6a9166facf8b505
Merch04 <kirill004mel@mail.ru>
2024-10-13 13:26:26 +0700

    change PhotoInterceptor.kt

--- app/src/main/java/com/example/mobile_development_lab_07/api/PhotoInterceptor.kt
+++ app/src/main/java/com/example/mobile_development_lab_07/api/PhotoInterceptor.kt
@@ -1,4 +1,29 @@
 package com.example.mobile_development_lab_07.api
 
-class PhotoInterceptor {
+import okhttp3.HttpUrl
+import okhttp3.Interceptor
+import okhttp3.Request
+import okhttp3.Response
+
+private const val API_KEY = "08d7a58a4f8a9d0e7842cc9caebcd60a"
+
+class PhotoInterceptor : Interceptor {
+    override fun intercept(chain: Interceptor.Chain)
+            : Response {
+        val originalRequest: Request =
+            chain.request()
+        val newUrl: HttpUrl =
+            originalRequest.url().newBuilder()
+                .addQueryParameter("api_key", API_KEY)
+                .addQueryParameter("format", "json")
+                .addQueryParameter("nojsoncallback", "1")
+                .addQueryParameter("extras", "url_s")
+                .addQueryParameter("safesearch", "1")
+                .build()
+        val newRequest: Request =
+            originalRequest.newBuilder()
+                .url(newUrl)
+                .build()
+        return chain.proceed(newRequest)
+    }
 }
