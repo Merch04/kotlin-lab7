d95ef697a79c7259c81043eec35b7aba35cdb057
Merch04 <kirill004mel@mail.ru>
2024-10-13 08:18:12 +0700

    update ThumbnailDownloader.kt

--- app/src/main/java/com/example/mobile_development_lab_07/ThumbnailDownloader.kt
+++ app/src/main/java/com/example/mobile_development_lab_07/ThumbnailDownloader.kt
@@ -1,4 +1,17 @@
 package com.example.mobile_development_lab_07
 
-class ThumbnailDownloader {
+import android.os.HandlerThread
+import android.util.Log
+
+private const val TAG = "ThumbnailDownloader"
+class ThumbnailDownloader<in T> : HandlerThread(TAG) {
+    private var hasQuit = false
+    override fun quit(): Boolean {
+        hasQuit = true
+        return super.quit()
+    }
+    fun queueThumbnail(target: T, url: String)
+    {
+        Log.i(TAG, "Got a URL: $url")
+    }
 }
